6096b5f60d3f01ea321984e970acb00a320c56f2
amaesa <49761004+amaesa@users.noreply.github.com>
2019-05-04 16:44:10 -0500

    Add files via upload

--- gamePlay.java
+++ gamePlay.java
@@ -1,8 +1,11 @@
 package sample;
 
+import java.lang.Math;
+
+
 public class gamePlay {
 
-    private int guessNumber// the number players have to guess
+    private int guessNumber;// the number players have to guess
 
     private int p1Guess;// guess of player 1
     private int p1Close;// how close is player 1
@@ -16,22 +19,75 @@ public class gamePlay {
     private int p4Guess;// guess of player 2
     private int p4Close;// how close is player 2
 
-    public void setPGuess(int player, int guess){
+    private boolean p1win ; // if player wins round
+    private boolean p2win ;
+    private boolean p3win;
+    private boolean p4win;
+
+    gamePlay(int p1,int p2, int p3, int p4){
+        p1Guess = p1;
+        p1Close = differnceFinder(guessNumber,p1Guess);
+
+        p2Guess = p2;
+        p2Close = differnceFinder(guessNumber,p2Guess);
+
+        p3Guess = p3;
+        p3Close = differnceFinder(guessNumber,p3Guess);
+
+        p4Guess = p4;
+        p4Close = differnceFinder(guessNumber,p4Guess);
+
+        p1win = false;
+        p2win = false;
+        p3win = false;
+        p4win = false;
+
+    }
+
+    private int differnceFinder(int random, int choice){
+        int temp;
+        temp = random - choice;
+        if(temp < 0){
+            temp = temp* (-1);
+        }
+        return temp;
+    }
+
+
+    private static int generateNumber(){// generation of random number
+        double temp = Math.random();//generates number in double form
+        temp = temp * 999;// generates number in range
+        int number = (int) temp;// makes number an int
+        return number;
+    }
+
+    public void makeNumber(){// use this function to generate number
+        guessNumber = generateNumber();
+
+    }
+
+
+
+    public void setPGuess(int player, int guess){// enter player number and thier guess
         switch(player){
             case 1:// set player 1 guess
                 p1Guess = guess;
+                p1Close = differnceFinder(guessNumber,p1Guess);
                 break;
 
             case 2:// set player 2 guess
                 p2Guess = guess;
+                p2Close = differnceFinder(guessNumber,p2Guess);
                 break;
 
             case 3:// set player 3 guess
                 p3Guess = guess;
+                p3Close = differnceFinder(guessNumber,p3Guess);
                 break;
 
             case 4:// set player 4 guess
                 p4Guess = guess;
+                p4Close = differnceFinder(guessNumber,p4Guess);
                 break;
 
             default:
@@ -44,28 +100,95 @@ public class gamePlay {
 
 
 
-    public int getPGuess(int player){
+    public int getPGuess(int player){// enter player number to get guess
         switch(player){
             case 1:// return player 1 guess
                 return p1Guess;
-                break;
 
             case 2:// return player 2 guess
                 return p2Guess;
-                break;
 
             case 3:// return player 3 guess
                 return p3Guess;
-                break;
 
             case 4:// return player 4 guess
                 return p4Guess;
-                break;
 
             default:
                 System.out.println(" Invalid Player");
-                break;
+                return (-1);
         }
     }
 
+
+    public int getPClose(int close){// to get how close a guess is put in the players guess you want
+        switch(close){
+            case 1:// return player 1 difference
+                return p1Close;
+
+            case 2:// return player 2 difference
+                return p2Close;
+
+            case 3:// return player 3 difference
+                return p3Close;
+
+            case 4:// return player 4 difference
+                return p4Close;
+
+            default:
+                System.out.println(" Invalid Player");
+                return (-1);
+        }
+    }
+
+    public int getGuessNumber(){
+        return guessNumber;
+    }
+
+    public void findExactWiner(){
+        if(p1Guess ==guessNumber){
+            p1win = true;
+        }
+        if(p2Guess ==guessNumber){
+            p2win = true;
+        }
+        if(p3Guess ==guessNumber){
+            p3win = true;
+        }
+        if(p4Guess ==guessNumber){
+            p4win = true;
+        }
+    }
+
+    public void findWinner(){
+        if(p1Close <=p2Close && p1Close <=p3Close && p1Close <=p4Close){
+            p1win = true;
+        }
+        if(p2Close <=p1Close && p2Close <=p3Close && p2Close <=p4Close){
+            p2win = true;
+        }
+        if(p3Close <=p2Close && p3Close <=p1Close && p3Close <=p4Close){
+            p3win = true;
+        }
+        if(p4Close <=p2Close && p4Close <=p3Close && p4Close <=p1Close){
+            p4win = true;
+        }
+    }
+
+    public boolean getWin(int player){// put player number to get thier win statues
+        if( player == 1){
+            return p1win;
+        }
+        if( player == 2){
+            return p2win;
+        }
+        if( player == 3){
+            return p3win;
+        }
+        if( player == 4){
+            return p4win;
+        }
+        return false;
+    }
 }
+
